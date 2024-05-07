ba04b590f69f9c0e5cbe91085b5a6500251209d7
Jonathan Aires <airesjonathan123@gmail.com>
2024-05-06 23:17:43 -0300

    refact: ensure that automation only quit after 5 seconds

--- outlook.py
+++ outlook.py
@@ -1,3 +1,4 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -13,47 +14,49 @@ options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 
 """ Getting the url to enter outlook """
-driver.get('https://outlook.live.com/mail/0/?cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c&nlp=1&RpsCsrfState=6d00a139-cf1a-9956-1f9c-9fa5ef8c7cb9&url=%2fowa%2f0%2f%3fcobrandid%253dab0455a0-8d03-46b9-b18b-df2f57b9e44c%2526nlp%253d1%2526RpsCsrfState%253d6d00a139-cf1a-9956-1f9c-9fa5ef8c7cb9')
-
-""" Setting selenium to wait 10 seconds before raising and exception """
-driver.implicitly_wait(60)  
-
-""" Login in Outlook """
-login = driver.find_element(By.XPATH, "//input[@name='loginfmt']")
-login.send_keys("ex4mple123@outlook.com")
-next_button = driver.find_element(By.XPATH, "//button[@id='idSIButton9']")
-next_button.click()
-password = driver.find_element(By.XPATH, "//input[@name='passwd']")
-password.send_keys("example123")
-enter_button = driver.find_element(By.XPATH, "//button[@id='idSIButton9']")
-enter_button.click()
-accept_button = driver.find_element(By.XPATH, "//button[@id='acceptButton']")
-accept_button.click()
-
-""" Accessing the button to send a new email """
 try:
-    new_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Novo email']")
-    new_email_button.click()
-except StaleElementReferenceException:
-    new_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Novo email']")
-    new_email_button.click()
+    driver.get('https://outlook.live.com/mail/0/?cobrandid=ab0455a0-8d03-46b9-b18b-df2f57b9e44c&nlp=1&RpsCsrfState=6d00a139-cf1a-9956-1f9c-9fa5ef8c7cb9&url=%2fowa%2f0%2f%3fcobrandid%253dab0455a0-8d03-46b9-b18b-df2f57b9e44c%2526nlp%253d1%2526RpsCsrfState%253d6d00a139-cf1a-9956-1f9c-9fa5ef8c7cb9')
 
-""" Accessing the destinatary """
-new_destinatary = driver.find_element(By.XPATH, "//div[@aria-label='Para']")
-new_destinatary.send_keys("airesjonathan123@outlook.com")
+    """ Setting selenium to wait 10 seconds before raising and exception """
+    driver.implicitly_wait(60)  
 
-""" Creating a topic """
-new_topic = driver.find_element(By.XPATH, "//input[@aria-label='Adicionar um assunto']")
-new_topic.send_keys("This is a test for automation")
+    """ Login in Outlook """
+    login = driver.find_element(By.XPATH, "//input[@name='loginfmt']")
+    login.send_keys("ex4mple123@outlook.com")
+    next_button = driver.find_element(By.XPATH, "//button[@id='idSIButton9']")
+    next_button.click()
+    password = driver.find_element(By.XPATH, "//input[@name='passwd']")
+    password.send_keys("example123")
+    enter_button = driver.find_element(By.XPATH, "//button[@id='idSIButton9']")
+    enter_button.click()
+    accept_button = driver.find_element(By.XPATH, "//button[@id='acceptButton']")
+    accept_button.click()
 
-""" Creating the message """
-new_message = driver.find_element(By.XPATH, "//div[@aria-label='Corpo da mensagem, pressione Alt+F10 para sair']")
-new_message.send_keys("Hi, this is an automated message")
+    """ Accessing the button to send a new email """
+    try:
+        new_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Novo email']")
+        new_email_button.click()
+    except StaleElementReferenceException:
+        new_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Novo email']")
+        new_email_button.click()
 
-""" Sending the email """
-send_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Enviar']")
-send_email_button.click()
+    """ Accessing the destinatary """
+    new_destinatary = driver.find_element(By.XPATH, "//div[@aria-label='Para']")
+    new_destinatary.send_keys("airesjonathan123@outlook.com")
 
-''' We can quit the driver if we want '''
+    """ Creating a topic """
+    new_topic = driver.find_element(By.XPATH, "//input[@aria-label='Adicionar um assunto']")
+    new_topic.send_keys("This is a test for automation")
 
-'''driver.quit()'''
+    """ Creating the message """
+    new_message = driver.find_element(By.XPATH, "//div[@aria-label='Corpo da mensagem, pressione Alt+F10 para sair']")
+    new_message.send_keys("Hi, this is an automated message")
+
+    """ Sending the email """
+    send_email_button = driver.find_element(By.XPATH, "//button[@aria-label='Enviar']")
+    send_email_button.click()
+
+finally:
+    time.sleep(5)
+    
+    driver.quit()
